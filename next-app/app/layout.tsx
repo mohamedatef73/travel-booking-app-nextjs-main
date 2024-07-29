f6431b61677ab8d51656cde6a0d79d4f89a03cc8
mohamedatef73 <matef4202@gmail.com>
2024-07-29 16:26:41 +0300

    fix

--- next-app/app/layout.tsx
+++ next-app/app/layout.tsx
@@ -1,21 +1,19 @@
 "use client"
-import Footer from '../components/Footer'
-import Navbar from '../components/Navbar'
-import './globals.css' // Import your global CSS file if you have one
+import Footer from "../components/Footer"
+import Navbar from "../components/Navbar"
+import "./globals.css" // Import your global CSS file if you have one
 
-export const metadata = {
-  title: 'Ceramic Store',
-  description: 'احدث منتجات وعروض شركة سيراميكا مايوركا',
-}
+// export const metadata = {
+//   title: 'Ceramic Store',
+//   description: 'احدث منتجات وعروض شركة سيراميكا مايوركا',
+// }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <head>
-        <title>{metadata.title}</title>
-      </head>
+      <head>{/* <title>{metadata.title}</title> */}</head>
       <body>
-        <Navbar/>
+        <Navbar />
         {children}
         <Footer />
       </body>
